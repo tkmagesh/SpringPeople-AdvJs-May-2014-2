7c287b0764cad6ad73254a02d93450411fbe5ba7
tkmagesh <tkmagesh77@gmail.com>
2014-05-23 16:54:51 +0530

    FP techniques - Seed - 2

--- Day-1/FunctionalProgramming/FPTechniques.js
+++ Day-1/FunctionalProgramming/FPTechniques.js
@@ -7,7 +7,48 @@ var products = [
 	{id : 5, name: "zen", cost : 60, units : 90, category : 1}
 ]
 
-function sort(){
-	//implementation to sort the products by id
+function sort(items,attrName){
+	for(var i=0;i<items.length-1;i++)
+		for(var j=i+1;j<items.length;j++){
+			if (items[i][attrName] > items[j][attrName]){
+				var temp = items[i];
+				items[i] = items[j];
+				items[j] = temp;
+			}
+		}
 }
 
+function sort(items,comparerFn){
+	for(var i=0;i<items.length-1;i++)
+		for(var j=i+1;j<items.length;j++){
+			if (comparerFn(items[i], items[j]) > 0){
+				var temp = items[i];
+				items[i] = items[j];
+				items[j] = temp;
+			}
+		}
+}
+
+function productComparerByValue(p1,p2){
+  var leftValue = p1.units * p1.cost,
+      rightValue = p2.units * p2.cost;
+  if (leftValue > rightValue) return 1;
+  if (leftValue < rightValue) return -1;
+  return 0;
+}
+
+function sort(items,comparer){
+	var comparerFn = typeof comparer === "function" ? comparer : function(item1, item2){
+		if (item1[comparer] > item2[comparer]) return 1;
+		if (item1[comparer] < item2[comparer]) return -1;
+		return 0
+	};
+	for(var i=0;i<items.length-1;i++)
+		for(var j=i+1;j<items.length;j++){
+			if (comparerFn(items[i], items[j]) > 0){
+				var temp = items[i];
+				items[i] = items[j];
+				items[j] = temp;
+			}
+		}
+}
