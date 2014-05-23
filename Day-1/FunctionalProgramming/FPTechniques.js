25adbae3507ad5e7836ddf332ded1462b2f3224d
tkmagesh <tkmagesh77@gmail.com>
2014-05-23 16:25:44 +0530

    FP techniques - seed

---
+++ Day-1/FunctionalProgramming/FPTechniques.js
@@ -0,0 +1,13 @@
+var products = [
+	{id : 6, name: "pen", cost : 70, units : 10, category : 1},
+	{id : 3, name: "hen", cost : 40, units : 60, category : 2},
+	{id : 9, name: "ten", cost : 30, units : 30, category : 1},
+	{id : 2, name: "len", cost : 50, units : 40, category : 2},
+	{id : 7, name: "den", cost : 20, units : 20, category : 2},
+	{id : 5, name: "zen", cost : 60, units : 90, category : 1}
+]
+
+function sort(){
+	//implementation to sort the products by id
+}
+
