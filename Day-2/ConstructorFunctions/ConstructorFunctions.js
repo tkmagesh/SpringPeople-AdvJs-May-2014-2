f0eccb1e8f1387ef77dc528d5b80edde8a407431
tkmagesh <tkmagesh77@gmail.com>
2014-05-24 10:55:49 +0530

    Constructor Function - Assignment - Solution

--- Day-2/ConstructorFunctions/ConstructorFunctions.js
+++ Day-2/ConstructorFunctions/ConstructorFunctions.js
@@ -9,3 +9,22 @@ function Employee(id,firstName,salary){
 	2. firstName cannot be assigned an empty string
 	3. salary cannot be assigned a value less than the current salary
 */
+
+function Employee(id,firstName,salary){
+  var _id = id,
+      _firstName = firstName,
+      _salary = salary;
+
+  this.id = function(){
+     return _id;
+  };
+  this.firstName = function(val){
+     if (typeof val === "undefined") return _firstName;
+     if (val !== "") _firstName = val;
+  };
+  this.salary = function(val){
+     if (typeof val === "undefined") return _salary;
+     if (val > _salary) _salary = val;
+  }
+}
+     
